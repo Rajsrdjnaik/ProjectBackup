be33c88c8868f95aca4c5bdf87cc4fa11fd6d7be
RAJAT <rajsrdjnaik@gmail.com>
2018-08-23 16:16:52 +0530

    Finally a backup

---
+++ Angular-project/src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import {NgModule} from '@angular/core';
+import {Routes , RouterModule, Router} from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './guards/auth.guard';
+import { PortfoliosComponent } from './portfolios/portfolios.component';
+import { VisitingCardComponent } from './portfolios/visiting-card/visiting-card.component';
+
+const routes: Routes = [
+  {path: '', component: ProfileComponent},
+  {path: 'portfolios', component: PortfoliosComponent ,canActivate: [AuthGuard]},
+  {path: 'register', component: RegistrationComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'portfolios/visiting', component: VisitingCardComponent, canActivate: [AuthGuard]}
+
+];
+
+@NgModule({
+    imports: [
+      RouterModule.forRoot(routes)
+    ],
+    exports: [RouterModule]
+  })
+
+  export class AppRoutingModule {}
+  export const routingComponents = [ProfileComponent, PortfoliosComponent, VisitingCardComponent, RegistrationComponent, LoginComponent];
